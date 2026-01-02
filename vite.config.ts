63cc5022e9aaa45b995621c792e1c2fba2a4362e
regesnaldo <reginaldodealmeida@gmail.com>
2026-01-02 08:28:56 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+// Este comando 'base' é o que faz o site aparecer no seu link do GitHub
 export default defineConfig({
   plugins: [react()],
   base: '/A.I-KIDS-LABS-NOVA-VERS-O/',
